e9bf82f35da2cfcf7b0ecbbddbe198c55490e889
Carlos Lassance <cadurosar@gmail.com>
2018-02-13 16:11:04 +0100

    fix cifar defferard batch size

--- defferard/cifar.py
+++ defferard/cifar.py
@@ -71,7 +71,7 @@ transform_test = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch, shuffle=True, num_workers=2)
 testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
 
